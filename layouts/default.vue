04d0c505a3423aff114346223cf2041f35b3d76d
marquillier <nicolas.marquillier@elosi.com>
2024-05-16 18:35:26 +0200

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <VApp>
+            <slot />
+            <VMain></VMain>
+        </VApp>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <VAppBar>
+            <VAppBarTitle>My App</VAppBarTitle>
+        </VAppBar>
+        <!-- <VNavigation-drawer></VNavigation-drawer> -->
+        <v-container fixed>
+            <h2>DashBoard</h2>
+            <p>Bienvenue sur le dashboard</p>            
+        </v-container>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.v-container {
+  position: sticky;
+  margin-top: 60px; /* Ajuster la valeur en fonction de la hauteur de la barre d'applis */
+}
+</style>
